7629f993f71e9f29e1b1e232c1858cc8a02336f9
bannyjuya-arch <bannyjuya@gmail.com>
2026-08-21 22:10:42 +0900

    Render rent benchmark panel in Part 3 control

--- app/control/layout.tsx
+++ app/control/layout.tsx
@@ -1,11 +1,13 @@
 import ReportLauncher from "./ReportLauncher";
 import CommercialAllocationTable from "./CommercialAllocationTable";
+import RentBenchmarkPanel from "./RentBenchmarkPanel";
 
 export default function ControlLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
       {children}
       <CommercialAllocationTable />
+      <RentBenchmarkPanel />
       <ReportLauncher />
     </>
   );
