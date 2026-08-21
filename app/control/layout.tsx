d963f1f320a039452fe484c6c59314350e2c6866
bannyjuya-arch <bannyjuya@gmail.com>
2026-08-21 21:22:40 +0900

    Show commercial allocation table in Part 2 flow

--- app/control/layout.tsx
+++ app/control/layout.tsx
@@ -1,9 +1,11 @@
 import ReportLauncher from "./ReportLauncher";
+import CommercialAllocationTable from "./CommercialAllocationTable";
 
 export default function ControlLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
       {children}
+      <CommercialAllocationTable />
       <ReportLauncher />
     </>
   );
